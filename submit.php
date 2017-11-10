42ae9d32318445a85c2bedcf8463f321ec056e1b
Patrick <patnavarro14@gmail.com>
2017-11-10 14:49:14 -0600

    echos form data

---
+++ submit.php
@@ -0,0 +1,8 @@
+<?php
+  echo $_POST['first_name'] ."<br />";
+  echo $_POST['last_name'] ."<br />";
+  echo $_POST['email'] ."<br />";
+  echo $_POST['phone'] ."<br />";
+  echo "==============================<br />";
+  echo "All Data Submitted Successfully!";
+?>
